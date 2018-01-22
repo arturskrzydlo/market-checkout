ca2c0cee5717aeead3ae40e6b0474f7c736ece8e
artur.skrzydlo <artur.skrzydlo@dieboldnixdorf.com>
2018-01-22 18:41:51 +0100

    Added acceptance test for product scanning and checking id receipt has been updated

--- src/test/java/com/pragmaticcoders/checkout/checkoutcomponent/checkout/ScanningProductsAcceptSpec.groovy
+++ src/test/java/com/pragmaticcoders/checkout/checkoutcomponent/checkout/ScanningProductsAcceptSpec.groovy
@@ -1,5 +1,7 @@
 package com.pragmaticcoders.checkout.checkoutcomponent.checkout
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.pragmaticcoders.checkout.checkoutcomponent.products.ProductService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -9,10 +11,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import spock.lang.Specification
 
+import javax.transaction.Transactional
 import java.lang.Void as Should
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -22,7 +24,13 @@ class ScanningProductsAcceptSpec extends Specification {
     private MockMvc mockMvc
 
     @Autowired
-    private ReceiptService productService
+    private ReceiptService receiptService
+
+    @Autowired
+    private ObjectMapper mapper
+
+    @Autowired
+    private ProductService productService
 
 
     Should "user can sucessfully open new receipt"() {
@@ -37,7 +45,40 @@ class ScanningProductsAcceptSpec extends Specification {
                     .andExpect(jsonPath('$.payment').doesNotExist())
     }
 
+    @Transactional
+    Should "user can sucessfully scan product, which return actual product price and receipt is updated with this product"() {
+        given: "product to scan"
+            def productForScanning = new ScannedProductDTO()
+            productForScanning.productName = "toothbrush"
+            productForScanning.quantity = 2
+        and: "existing empty receipt"
+            def receipt = createEmptyReceipt()
+            receiptService.produceReceiptWithPayment(receipt.getId()).getItems()
+        and: "existing product in stock"
+            def productInStock = createNewProduct(productForScanning.productName, 5.0)
+        when:
+            def result = mockMvc.perform(MockMvcRequestBuilders.patch("/receipt/" + receipt.id)
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .content(mapper.writeValueAsString(productForScanning)))
+        then: "produtct with its normal price is returned"
+            result.andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                    .andExpect(jsonPath('$.price').value(productInStock * productForScanning.quantity))
+        and: "receipt list has been updated and it's size is now equals to one"
+            receiptService.produceReceiptWithPayment(receipt.getId()).getItems().size() == 1
+
+    }
+
 /*    Should "can back to receipt, change it and do calculation once again until receipt exists in a system"(){
 
     }*/
+
+    def createNewProduct(String name, Double price) {
+        productService.createProduct(name, price)
+    }
+
+    def createEmptyReceipt() {
+        receiptService.createNewReceipt()
+    }
+
 }
